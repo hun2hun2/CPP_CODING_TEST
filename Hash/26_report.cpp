a848e0f6bee34186edc691faf2fe4b6fa84ceecb
hun2hun2 <aa09302003@gmail.com>
2026-02-05 19:58:45 +0900

    add source codes 22~25

---
+++ Hash/26_report.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+#include <vector>
+#include <string>
+#include <sstream>
+
+using namespace std;
+
+vector<int> solution(vector<string> id_list, vector<string> report, int k) {
+    
+}
